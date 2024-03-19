a5dbb1fabd8dc4173b5afb37fd3a9b7f6b803008
marianmna <solovevamaria242@gmail.com>
2024-03-19 22:26:12 +0700

    change1

--- keys.py
+++ keys.py
@@ -1,1 +1,12 @@
+# Case-study #3
+# Developers: Borodin Artemiy, Solovyova Maria,
+# Selikhova Polina, Lyamin Egor
+#
+
+import ru_local as ru
+
 print('Hello, Harry!')
+
+
+if __name__ == '__main__':
+    main()
